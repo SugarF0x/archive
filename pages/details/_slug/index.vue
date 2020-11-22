72aede71b29dd04e2ac0d099941e4d212a23b5bb
Sugar_F0x <sugarfox.dev@gmail.com>
2020-11-22 17:38:10 +0300

    :white_check_mark: Add base for description page

---
+++ pages/details/_slug/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <article>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'description-slug',
+  data() {
+    return {
+      page: undefined
+    }
+  },
+  async asyncData ({ $content, params }) {
+    const page = await $content(params.slug).fetch()
+
+    return {
+      page
+    }
+  }
+})
+</script>
