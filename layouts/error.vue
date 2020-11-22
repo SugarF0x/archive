aedafbb55b11bfaae3d80fd02778b4d602b9cdc2
Sugar_F0x <sugarfox.dev@gmail.com>
2020-11-22 17:47:55 +0300

    :art: Update error page style

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,19 +1,47 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <v-layout column
+            justify-center
+            align-center
+  >
+    <v-card class="d-inline-flex flex-column text-center pa-2">
+      <h1 class="display-3">
+          {{ error.statusCode }} - {{ error.message }}
+      </h1>
+      <v-card-text>
+        <p v-if="error.config" class="text-left mt-5 pa-2 secondary white--text">{{ error.config }}</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn nuxt
+               to="/"
+               color="blue darken-2"
+        >
+          <v-icon left>mdi-home</v-icon>
+          Home Page
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn @click="reload"
+               color="blue darken-2"
+               v-if="error.statusCode !== 404"
+        >
+          Reload
+          <v-icon right>mdi-update</v-icon>
+        </v-btn>
+        <v-btn @click="$router.go(-1)"
+               color="blue darken-2"
+               v-else
+        >
+          Go back
+          <v-icon right>mdi-undo-variant</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-layout>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
   layout: 'empty',
   props: {
     error: {
@@ -21,20 +49,17 @@ export default {
       default: null
     }
   },
-  data () {
+  head () {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      title: this.error.statusCode + ' ' + this.error.message
     }
   },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
+  methods: {
+    reload() {
+      document.location.reload()
     }
   }
-}
+})
 </script>
 
 <style scoped>
