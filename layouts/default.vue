f3e3add400217325c1152aa87e113dd68bb24457
Sugar_F0x <sugarfox.dev@gmail.com>
2020-11-22 23:21:05 +0300

    :white_check_mark: Add toolbar title home route
    > Add toolbar title click route to home
    > Add SGFX logo
    > Add kurbanistika font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <template>
   <v-app dark >
     <v-app-bar app >
-      <v-toolbar-title v-text="'Archives'" />
+      <router-link to="/" style="text-decoration: none; color: inherit;">
+        <v-row justify="center" align="center">
+           <v-img src="/sgfx_line.webp" height="2.5rem" width="2.5rem" contain></v-img>
+          <v-toolbar-title id="logo">
+            Archives
+          </v-toolbar-title>
+        </v-row>
+      </router-link>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -16,3 +23,13 @@
   </v-app>
 </template>
 
+<style lang="less" scoped>
+@font-face {
+  font-family: 'Kurbanistika';
+  src: url('/fonts/kurbanistika.otf');
+}
+#logo {
+  font-family: Kurbanistika, sans-serif;
+  font-size: 2.5rem;
+}
+</style>
